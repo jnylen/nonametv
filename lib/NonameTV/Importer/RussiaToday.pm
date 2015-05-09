662909a527d662d0809711bbe94abf9c3174479f
Joakim Nylen <me@jnylen.nu>
2015-05-09 05:02:05 +0200

    Fix Russia Today

--- lib/NonameTV/Importer/RussiaToday.pm
+++ lib/NonameTV/Importer/RussiaToday.pm
@@ -4,166 +4,147 @@ use strict;
 use warnings;
 
 =pod
-Importer for Global Listings (globalistings.info)
 
-Channels: Russia Today (Int. and Russia)
 
-Every month is runned as a seperate batch.
 
 =cut
 
-use utf8;
 
-use POSIX;
 use DateTime;
-use XML::LibXML;
-use Spreadsheet::ParseExcel;
+use Date::Parse;
+use File::Slurp;
+use Encode;
 use Data::Dumper;
-use Spreadsheet::Read;
+use Scalar::Util qw(looks_like_number);
 
-use Spreadsheet::XLSX;
-use Spreadsheet::XLSX::Utility2007 qw(ExcelFmt ExcelLocaltime LocaltimeExcel);
-use Spreadsheet::Read;
-
-use Text::Iconv;
-my $converter = Text::Iconv -> new ("utf-8", "windows-1251");
-
-use NonameTV qw/norm MonthNumber/;
+use NonameTV qw/MyGet expand_entities AddCountry AddCategory norm/;
 use NonameTV::DataStore::Helper;
 use NonameTV::Log qw/progress error/;
-use NonameTV::Config qw/ReadConfig/;
 
 use NonameTV::Importer::BaseFile;
 
 use base 'NonameTV::Importer::BaseFile';
 
 sub new {
-  my $proto = shift;
-  my $class = ref($proto) || $proto;
-  my $self  = $class->SUPER::new( @_ );
-  bless ($self, $class);
-
-  my $conf = ReadConfig();
-
-  $self->{FileStore} = $conf->{FileStore};
-
-  my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore}, "Europe/Moscow" );
-  $self->{datastorehelper} = $dsh;
-
-  $self->{datastore}->{augment} = 1;
-
-  return $self;
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self  = $class->SUPER::new( @_ );
+    bless ($self, $class);
+    my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore}, "Europe/Moscow" );
+    $self->{datastorehelper} = $dsh;
+  	
+    return $self;
 }
 
-sub ImportContentFile {
-  my $self = shift;
-  my( $file, $chd ) = @_;
-
-  $self->{fileerror} = 0;
-
-  if( $file =~ /\.xls$/i ){
-    $self->ImportXLS( $file, $chd );
-  } else {
-    error( "RussiaToday: Unknown file format: $file" );
-  }
-
-  return;
-}
-
-sub ImportXLS
+sub ImportContentFile
 {
   my $self = shift;
+
   my( $file, $chd ) = @_;
 
-  my $dsh = $self->{datastorehelper};
   my $ds = $self->{datastore};
+  my $dsh = $self->{datastorehelper};
 
-  my %columns = ();
-  my $date;
+
+  my $cref = read_file( $file ) ;
+  progress("Processing $file..");
+
+  my $str = decode( "utf-16", $cref );
+  
+  my @rows = split(/\r\n|\n|\r/, $str );
+
+  if( scalar( @rows < 2 ) )
+  {
+    error( "$file: No data found" );
+    return 0;
+  }
+
+  my $columns = [ split( "\t", $rows[1] ) ];
+  my $date = "";
   my $currdate = "x";
-  my $oBook;
 
-  if ( $file =~ /\.xlsx$/i ){ progress( "using .xlsx" );  $oBook = Spreadsheet::XLSX -> new ($file, $converter); }
-  else { $oBook = Spreadsheet::ParseExcel->new()->Parse( $file );  }
-  progress("RussiaToday: Processing $file");
+  # Couldnt use column name for some reason
+  for ( my $i = 2; $i < scalar @rows; $i++ )
+  {
+    my $inrow = $self->row_to_hash($file, $rows[$i], $columns );
 
-  # fields
-  my $num_date = 0;
-  my $num_starttime = 1;
-  my $num_endtime = 2;
-  my $num_title = 3;
-  my $num_genre = 5;
-  my $num_desc = 6;
-
-  # main loop
-  #for(my $iSheet=0; $iSheet < $oBook->worksheet_count() ; $iSheet++) {
-  foreach my $oWkS (@{$oBook->{Worksheet}}) {
-    #my $oWkS = $oBook->{Worksheet}[$iSheet];
-    progress( "RussiaToday: $chd->{xmltvid}: Processing worksheet: $oWkS->{Name}" );
-
-    # browse through rows
-    for(my $iR = 2 ; defined $oWkS->{MaxRow} && $iR <= $oWkS->{MaxRow} ; $iR++) {
-
-      # date - column 0 ('Date')
-      my $oWkC = $oWkS->{Cells}[$iR][$num_date];
-      next if( ! $oWkC );
-      next if( ! $oWkC->Value );
-      $date = ParseDate( $oWkC->Value );
-      next if( ! $date );
-
-	  # Startdate
-      if( $date ne $currdate ) {
-      	if( $currdate ne "x" ) {
-			# save last day if we have it in memory
-		#	FlushDayData( $channel_xmltvid, $dsh , @ces );
-			$dsh->EndBatch( 1 );
-        }
-
-      	my $batchid = $chd->{xmltvid} . "_" . $date;
-        $dsh->StartBatch( $batchid , $chd->{id} );
-        progress("RussiaToday: $chd->{xmltvid}: Date is $date");
-        $dsh->StartDate( $date , "00:00" );
-        $currdate = $date;
+    $date = ParseDate($inrow->{"Date"});
+    next if !defined($date);
+    if ($date ne $currdate) {
+      if( $currdate ne "x" ) {
+	    $dsh->EndBatch( 1 );
       }
 
-	  # time
-	  $oWkC = $oWkS->{Cells}[$iR][$num_starttime];
-      next if( ! $oWkC );
-      my $time = ParseTime($oWkC->Value) if( $oWkC->Value );
+      my $batchid = $chd->{xmltvid} . "_" . $date;
+      $dsh->StartBatch( $batchid , $chd->{id} );
+      progress("RT: $chd->{xmltvid}: Date is $date");
+      $dsh->StartDate( $date , "00:00" );
+      $currdate = $date;
+    }
 
-      # title
-      $oWkC = $oWkS->{Cells}[$iR][$num_title];
-      next if( ! $oWkC );
-      my $title = $oWkC->Value if( $oWkC->Value );
+    my $title = norm( $inrow->{'Program Name/Main Program Title'} );
+    my $start = norm( $inrow->{'Start'} );
+    my $end   = norm( $inrow->{'End'} );
+    my $desc  = norm( $inrow->{'Generic Program Synopsis'} );
 
-	  # extra info
-	  my $desc = $oWkS->{Cells}[$iR][$num_desc]->Value if $oWkS->{Cells}[$iR][$num_desc];
+    my $ce = {
+      channel_id => $chd->{id},
+      title => $title,
+      description => $desc,
+      start_time => $start,
+      end_time => $end,
+    };
 
-      progress("RussiaToday: $chd->{xmltvid}: $time - $title");
+    my $genre = norm( $inrow->{'Program Genre'} );
+    if(defined($genre) and $genre ne "") {
+    	my($program_type, $category ) = $ds->LookupCat( 'RussiaToday', $genre );
+			AddCategory( $ce, $program_type, $category );
+    }
 
-      my $ce = {
-        channel_id => $chd->{channel_id},
-        title => norm( $title ),
-        start_time => $time,
-        description => norm( $desc ),
-      };
+    progress( "RT: $chd->{xmltvid}: $start - ".$ce->{title} );
 
-      $dsh->AddProgramme( $ce );
+    $dsh->AddProgramme( $ce );
+  }
 
-    } # next row
-  } # next worksheet
+  $dsh->EndBatch( 1 );
 
-	$dsh->EndBatch( 1 );
-
+  # Success
   return 1;
+}
+
+sub row_to_hash
+{
+  my $self = shift;
+  my( $batch_id, $row, $columns ) = @_;
+  #$row =~ s/\t.$//;
+ # if $(row)
+  my @coldata = split( "\t", $row );
+  my %res;
+
+  
+  if( scalar( @coldata ) > scalar( @{$columns} ) )
+  {
+    error( "$batch_id: Too many data columns " .
+           scalar( @coldata ) . " > " .
+           scalar( @{$columns} ) );
+  }
+
+  for( my $i=0; $i<scalar(@coldata) and $i<scalar(@{$columns}); $i++ )
+  {
+    my $column = $columns->[$i];
+
+    $res{norm($column)} = norm($coldata[$i])
+      if $coldata[$i] =~ /\S/; 
+  }
+
+  return \%res;
 }
 
 sub ParseDate {
   my( $text ) = @_;
 
   $text =~ s/^\s+//;
-
-  #print("text: $text\n");
+  #print(">$text<\n");
 
   my( $dayname, $day, $monthname, $year );
   my $month;
@@ -176,19 +157,9 @@ sub ParseDate {
 
   $year += 2000 if $year < 100;
 
+  return undef if !defined($year);
+
   return sprintf( '%d-%02d-%02d', $year, $month, $day );
-}
-
-sub ParseTime
-{
-  my ( $text ) = @_;
-  #print("Text: $text\n");
-
-  my( $hour, $min ) = ( $text =~ /(\d+):(\d+)/ );
-
-  #return undef if( ! $hour or ! $min );
-
-  return sprintf( "%02d:%02d", $hour, $min );
 }
 
 1;
