f81acd308e537e2ba3de6dad206b1b66f14a515e
Joakim Nylen <me@jnylen.nu>
2016-05-25 17:01:13 +0000

    Try to fix this

--- lib/NonameTV/Importer/DR_xml.pm
+++ lib/NonameTV/Importer/DR_xml.pm
@@ -16,7 +16,7 @@ use DateTime;
 use XML::LibXML;
 use Roman;
 
-use NonameTV qw/ParseXml AddCategory AddCountry norm/;
+use NonameTV qw/ParseXml AddCategory AddCountry norm ParseDescCatDan/;
 use NonameTV::DataStore::Helper;
 use NonameTV::Log qw/w f p/;
 
@@ -36,7 +36,7 @@ sub new {
     $self->{UrlRoot} = 'http://www.dr.dk/Tjenester/epglive/epg.';
   }
 
-  my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore}, "Europe/Copenhagen" );
+  my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore} );
   $self->{datastorehelper} = $dsh;
   $self->{NO_DUPLICATE_SKIP} = 1;
   $self->{datastore}->{augment} = 1;
@@ -110,11 +110,11 @@ sub ImportContent {
     my $start = ParseDateTime( $b->findvalue( "pro_publish[1]/ppu_start_timestamp_announced" ) );
 
     if( $start->ymd("-") ne $currdate ){
-		p("Date is ".$start->ymd("-"));
+  		p("Date is ".$start->ymd("-"));
 
-		$dsh->StartDate( $start->ymd("-") , "00:00" );
-		$currdate = $start->ymd("-");
-	}
+  		#$dsh->StartDate( $start->ymd("-") , "06:00" );
+  		$currdate = $start->ymd("-");
+  	}
 
     my $title = $b->findvalue( "pro_title" );
     my $title_alt = $b->findvalue( "pro_publish[1]/ppu_title_alt" );
@@ -133,28 +133,43 @@ sub ImportContent {
     my $desc = $b->findvalue( "pro_publish[1]/ppu_description" );
     my $genre = $b->findvalue( "prd_genre_text" );
 
-	# Cleanup
-	$title =~ s/Fredagsfilm: //i;
-	$title =~ s/Dokumania: //i;
-
 	# Put everything in a array
     my $ce = {
       channel_id => $chd->{id},
-      start_time => $start->hms(":"),
+      start_time => $start->ymd("-") . " " . $start->hms(":"),
       title => norm($title),
       description => norm($desc),
-      subtitle	  => norm($subtitle),
     };
 
-	  # Episode info in xmltv-format
-      if( ($episode ne "") and ( $of_episode ne "") )
-      {
-        $ce->{episode} = sprintf( ". %d/%d .", $episode-1, $of_episode );
-      }
-      elsif( $episode ne "" )
-      {
-        $ce->{episode} = sprintf( ". %d .", $episode-1 );
-      }
+    my $extra = {};
+    $extra->{qualifiers} = [];
+
+    $ce->{subtitle} = norm($subtitle) if norm($subtitle) ne "";
+
+    # Episode info in xmltv-format
+    if( ($episode ne "") and ( $of_episode ne "") ) {
+      $ce->{episode} = sprintf( ". %d/%d .", $episode-1, $of_episode );
+      $ce->{program_type} = "series";
+    } elsif( $episode ne "" ) {
+      $ce->{episode} = sprintf( ". %d .", $episode-1 );
+      $ce->{program_type} = "series";
+    }
+
+
+    # Movie.
+    if($ce->{title} =~ /^(Natbio|Filmperler|Fredagsfilm)\:/i) {
+      $ce->{program_type} = "movie";
+    }
+
+    # Cleanup
+    $ce->{title} =~ s/\(G\)$//i;
+    $ce->{title} =~ s/Fredagsfilm: //i;
+    $ce->{title} =~ s/Dokumania: //i;
+    $ce->{title} =~ s/Filmperler: //i;
+    $ce->{title} =~ s/Natbio: //i;
+    $ce->{title} =~ s/Dokland: //i;
+    $ce->{title} =~ s/\.$//i;
+    $ce->{title} = norm($ce->{title});
 
     my($country2 ) = $ds->LookupCountry( "DR", norm($country) );
     AddCountry( $ce, $country2 );
@@ -162,33 +177,31 @@ sub ImportContent {
     $ce->{aspect} = '4:3';
 
     my $widescreen =  $b->findvalue( 'pro_publish[1]/ppu_video' );
-	if( $widescreen eq '16:9' ){
-   	 	$ce->{aspect} = '16:9';
-	}
-	if( $widescreen eq 'HD' ){
-   	 	$ce->{quality} = "HDTV";
-	}
+    if( $widescreen eq '16:9' ){
+     	 	$ce->{aspect} = '16:9';
+        push $extra->{qualifiers}, "widescreen";
+  	} elsif( $widescreen eq 'HD' ){
+     	 	$ce->{quality} = "HDTV";
+        push $extra->{qualifiers}, "HD";
+  	}
 
-	my $live = $b->findvalue( 'pro_publish[1]/ppu_islive' );
-	if( $live eq "TRUE" )
-	{
-		$ce->{live} = "1";
-	}
-	else
-	{
-		$ce->{live} = "0";
-	}
+  	my $live = $b->findvalue( 'pro_publish[1]/ppu_islive' );
+  	if( $live eq "TRUE" ) {
+  		$ce->{live} = "1";
+      push $extra->{qualifiers}, "live";
+  	} else {
+  		$ce->{live} = "0";
+  	}
 
-	my $rerun = $b->findvalue( 'pro_publish[1]/ppu_isrerun' );
-	if( $rerun eq "TRUE" )
-	{
-		$ce->{rerun} = "1";
-	}
-	else
-	{
-		$ce->{rerun} = "0";
-	}
+  	my $rerun = $b->findvalue( 'pro_publish[1]/ppu_isrerun' );
+  	if( $rerun eq "TRUE" ) {
+  		$ce->{rerun} = "1";
+      push $extra->{qualifiers}, "rerun";
+  	} else {
+  		$ce->{rerun} = "0";
+  	}
 
+    # Prod year
     $ce->{production_date} = "$year-01-01" if $year ne "";
 
     # Sometimes these production years differs through out the
@@ -198,101 +211,127 @@ sub ImportContent {
         $ce->{production_date} = "$1-01-01";
     }
 
+    # Category
     my($program_type, $category ) = $ds->LookupCat( 'DR', $genre );
-	AddCategory( $ce, $program_type, $category );
+    AddCategory( $ce, $program_type, $category );
 
-	## Arrays
-	my @actors;
+    ( $program_type, $category ) = ParseDescCatDan( $genretext );
+    AddCategory( $ce, $program_type, $category );
+
+    ## Arrays
+    my @actors;
     my @directors;
+    my @writers;
 
-	## Split the text, add directors and more.
-	my @sentences = (split_text( $ce->{description} ), "");
-	for( my $i=0; $i<scalar(@sentences); $i++ )
+    ## Split the text, add directors and more.
+    my @sentences = (split_text( $ce->{description} ), "");
+    for( my $i=0; $i<scalar(@sentences); $i++ )
     {
-		if( my( $role, $name ) = ($sentences[$i] =~ /^(.*)\:\s+(.*)./) )
-        {
-        	# If name is longer than 15 skip. Probably a fucked up text.
-        	if(length($name) > 15) {
-        		#print("Longer than 15.\n");
-        		next;
-        	}
-
-        	# Include the role
-			my $name_new = norm( $name )." (".norm($role).")";
-
-			if( $role =~ /Instruktion/i  ) {
-				# This should ONLY happened on the Instruktion one.
-				$name = parse_person_list($name);
-
-				# Director
-				push @directors, $name;
-
-				# Not a series?
-				if(!defined($ce->{episode})) {
-					# If this program has an director, it should be
-                	# a movie. If it isn't, please tag this DIRECTLY.
-                	$ce->{program_type} = 'movie';
-
-                	# Category removal
-                	if(defined($ce->{category}) and $ce->{category} eq "Series") {
-                    	$ce->{category} = undef;
-                    }
-				}
-			} else {
-				push @actors, $name_new;
-			}
-
-			$sentences[$i] = "";
+      if( my( $role, $name ) = ($sentences[$i] =~ /^(.*)\:\s+(.*)./) )
+      {
+        # If name is longer than 15 skip. Probably a fucked up text.
+        if(length($name) > 15) {
+          #print("Longer than 15.\n");
+          next;
         }
 
+        # Include the role
+        my $name_new = norm( $name )." (".norm($role).")";
+
+        if( $role =~ /Instruktion/i  ) {
+          # This should ONLY happened on the Instruktion one.
+          $name = parse_person_list($name);
+
+          # Director
+          push @directors, $name;
+
+          # Not a series?
+          if(!defined($ce->{episode})) {
+            # If this program has an director, it should be
+            # a movie. If it isn't, please tag this DIRECTLY.
+            $ce->{program_type} = 'movie';
+
+            # Category removal
+            if(defined($ce->{category}) and $ce->{category} eq "Series") {
+              $ce->{category} = undef;
+            }
+          }
+        }elsif( $role =~ /Manuskript/i  ) {
+          push @writers, $name;
+        } else {
+          push @actors, $name_new;
+        }
+
+        $sentences[$i] = "";
+      }
     }
 
-	$ce->{description} = join_text( @sentences );
+    # add new text
+    $ce->{description} = join_text( @sentences );
 
-	# Season and this is a series now.
-      if(defined($ce->{episode})) {
-      	my ( $original_title, $romanseason ) = ( $ce->{title} =~ /^(.*)\s+(.*)$/ );
+    # episodes is in the title
+    if(defined($ce->{episode})) {
+      my ( $original_title, $romanseason ) = ( $ce->{title} =~ /^(.*)\s+(.*)$/ );
 
-      	# Roman season found
-      	if(defined($romanseason) and isroman($romanseason)) {
-      		my $romanseason_arabic = arabic($romanseason);
+      # Roman season found
+      if(defined($romanseason) and isroman($romanseason)) {
+        my $romanseason_arabic = arabic($romanseason);
 
-      		$ce->{title} = norm($original_title);
+        $ce->{title} = norm($original_title);
 
-      		# Series
-      		$ce->{program_type} = "series";
-      		if(defined($ce->{category}) and $ce->{category} eq "Movies") {
-      			$ce->{category} = undef;
-      		}
+        # Series
+        $ce->{program_type} = "series";
+        if(defined($ce->{category}) and $ce->{category} eq "Movies") {
+          $ce->{category} = undef;
+        }
 
-      		$ce->{episode} = $romanseason_arabic-1 . $ce->{episode};
+        $ce->{episode} = $romanseason_arabic-1 . $ce->{episode};
 
-      	}
       }
+    }
 
+    # add actors
     if( scalar( @actors ) > 0 )
     {
-    	$ce->{actors} = join ";", @actors;
+      $ce->{actors} = join ";", @actors;
     }
 
+    # add directors
     if( scalar( @directors ) > 0 )
     {
-		$ce->{directors} = join ";", @directors;
+      $ce->{directors} = join ";", @directors;
+    }
+
+    # add writers
+    if( scalar( @writers ) > 0 )
+    {
+      $ce->{writers} = join ";", @writers;
     }
 
     # DR fucks Family guy up and tags every episode as a movie, wtf?
     if($ce->{title} eq "Family Guy") {
-    	$ce->{program_type} = "series";
-    	if(defined($ce->{category})) {
-        	$ce->{category} = undef;
+      $ce->{program_type} = "series";
+      if(defined($ce->{category})) {
+          $ce->{category} = undef;
         }
     }
 
-    $ce->{original_title} = norm($title_alt) if defined($title_alt) and $ce->{title} ne norm($title_alt) and norm($title_alt) ne ""; # Add original title
+    if( (defined($ce->{program_type}) and $ce->{program_type} ne "movie") and defined($title_alt) and my( $orgtit, $orgsubtit ) = ($title_alt =~ /^(.*)\:\s+(.*)$/) )
+    {
+      $ce->{original_title} = norm($orgtit) if defined($orgtit) and $ce->{title} ne norm($orgtit) and norm($orgtit) ne ""; # Add original title
+      $ce->{original_subtitle} = norm($orgsubtit) if defined($orgsubtit) and norm($orgsubtit) ne "";
+    } else {
+      $ce->{original_title} = norm($title_alt) if defined($title_alt) and $ce->{title} ne norm($title_alt) and norm($title_alt) ne "";
+    }
 
-	p($start." $ce->{title}");
+    $ce->{title} = "end-of-transmission" if $ce->{title} =~ /^Udsendelsesoph.*r/i;
+    $ce->{category} = "Movies" if defined($ce->{program_type}) and $ce->{program_type} eq "movie" and (defined($ce->{category}) and $ce->{category} eq "Series");
 
-    $dsh->AddProgramme( $ce );
+    p($start." $ce->{title}");
+
+    $ce->{extra} = $extra;
+
+    $ds->AddProgramme( $ce );
   }
 
   return 1;
@@ -313,7 +352,10 @@ sub ParseDateTime {
     hour => $hour,
     minute => $minute,
     second => $second,
-      );
+    time_zone => 'Europe/Copenhagen',
+  );
+
+  #$dt->set_time_zone( "UTC" );
 
   return $dt;
 }
