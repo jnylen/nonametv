dad94a7fa42d9d2f20f2a3c8f9ea72750e3a262c
Jnylen <me@jnylen.nu>
2017-01-12 02:03:52 +0100

    Trying to fig some thigns

--- lib/NonameTV/Importer/Trace.pm
+++ lib/NonameTV/Importer/Trace.pm
@@ -20,6 +20,13 @@ use DateTime::Format::Excel;
 use Data::Dumper;
 use File::Temp qw/tempfile/;
 
+use Spreadsheet::XLSX;
+use Spreadsheet::XLSX::Utility2007 qw(ExcelFmt ExcelLocaltime LocaltimeExcel int2col);
+use Spreadsheet::Read;
+
+use Text::Iconv;
+my $converter = Text::Iconv -> new ("utf-8", "windows-1251");
+
 use NonameTV qw/norm AddCategory MonthNumber/;
 use NonameTV::DataStore::Helper;
 use NonameTV::Log qw/progress error/;
@@ -35,16 +42,11 @@ sub new {
   my $self  = $class->SUPER::new( @_ );
   bless ($self, $class);
 
-  defined( $self->{UrlRoot} ) or die "You must specify UrlRoot";
-
-  $self->{MinMonths} = 1 unless defined $self->{MinMonths};
-  $self->{MaxMonths} = 12 unless defined $self->{MaxMonths};
-
   my $conf = ReadConfig();
 
   $self->{FileStore} = $conf->{FileStore};
 
-  my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore}, "Europe/Zagreb" );
+  my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore}, "Europe/Stockholm" );
   $self->{datastorehelper} = $dsh;
 
   return $self;
@@ -56,15 +58,11 @@ sub ImportContentFile {
 
   $self->{fileerror} = 0;
 
-  my $channel_id = $chd->{id};
-  my $channel_xmltvid = $chd->{xmltvid};
   my $dsh = $self->{datastorehelper};
   my $ds = $self->{datastore};
 
-#return if( $file !~ /AUGUST/ );
-
-  if( $file =~ /\.xls$/i ){
-    $self->ImportXLS( $file, $channel_id, $channel_xmltvid );
+  if( $file =~ /\.xlsx$/i ){
+    $self->ImportXLS( $file, $chd );
   }
 
   return;
@@ -73,7 +71,7 @@ sub ImportContentFile {
 sub ImportXLS
 {
   my $self = shift;
-  my( $file, $channel_id, $channel_xmltvid ) = @_;
+  my( $file, $chd ) = @_;
 
   my $dsh = $self->{datastorehelper};
   my $ds = $self->{datastore};
@@ -82,29 +80,41 @@ sub ImportXLS
   my $date;
   my $currdate = "x";
 
-  progress( "Trace XLS: $channel_xmltvid: Processing XLS $file" );
+  progress( "Trace XLS: $chd->{xmltvid}: Processing XLS $file" );
 
   my( $oBook, $oWkS, $oWkC );
-  $oBook = Spreadsheet::ParseExcel::Workbook->Parse( $file );
+  if ( $file =~ /\.xlsx$/i ){ progress( "using .xlsx" );  $oBook = Spreadsheet::XLSX -> new ($file, $converter); }
+  else { $oBook = Spreadsheet::ParseExcel::Workbook->Parse( $file );  }
+  my $ref = ReadData ($file);
 
   if( not defined( $oBook ) ) {
     error( "Trace XLS: $file: Failed to parse xls" );
     return;
   }
 
+  my $gmt = "no";
+
   for(my $iSheet=0; $iSheet < $oBook->{SheetCount} ; $iSheet++) {
+    my $oWkS = $oBook->{Worksheet}[$iSheet];
+    my $foundcolumns = 0;
 
-    $oWkS = $oBook->{Worksheet}[$iSheet];
 
-    if( $oWkS->{Name} !~ /^GMT \+1$/ ){
-      progress("Trace XLS: $channel_xmltvid: skipping worksheet named '$oWkS->{Name}'");
+    if( $oWkS->{Name} =~ /^GMT/ ){
+      $gmt = "true";
+    }
+
+    if( $gmt eq "true" and $oWkS->{Name} !~ /^GMT \+1$/ and $oWkS->{Name} !~ /^GMT\+1$/ ){
+      progress("Trace XLS: $chd->{xmltvid}: skipping worksheet named '$oWkS->{Name}'");
       next;
     }
 
-    progress("Trace XLS: $channel_xmltvid: processing worksheet named '$oWkS->{Name}'");
+    progress("Trace XLS: $chd->{xmltvid}: processing worksheet named '$oWkS->{Name}'");
+
+    my $i = 0;
 
     # read the rows with data
     for(my $iR = $oWkS->{MinRow} ; defined $oWkS->{MaxRow} && $iR <= $oWkS->{MaxRow} ; $iR++) {
+      $i++;
 
       if( not %columns ){
 
@@ -113,86 +123,153 @@ sub ImportXLS
         # for further findvalue() calls
         for(my $iC = $oWkS->{MinCol} ; defined $oWkS->{MaxCol} && $iC <= $oWkS->{MaxCol} ; $iC++) {
           if( $oWkS->{Cells}[$iR][$iC] ){
-            $columns{$oWkS->{Cells}[$iR][$iC]->Value} = $iC;
+            my $column_name = norm($oWkS->{Cells}[$iR][$iC]->Value);
+            print("$iC - '$column_name'\n");
+
+            $columns{'Title'} = $iC if( $column_name =~ /^programmeName$/i );
+            $columns{'Title'} = $iC if( $column_name =~ /^Programme Name$/i );
+            $columns{'Title'} = $iC if( $column_name =~ /^﻿Programme Title$/i );
+            $columns{'Title'} = $iC if( $column_name =~ /^Program Title$/i );
+            $columns{'Title'} = $iC if( $column_name =~ /^Nom du programme/i );
+            $columns{'Title'} = $iC if( !defined($columns{'Title'}) and $column_name eq "" );
+
+            $columns{'EpTitle'} = $iC if( $column_name =~ /^Episode Title$/i );
+            $columns{'Date'} = $iC if( $column_name =~ /^Programme Start Date$/i );
+            $columns{'Date'} = $iC if( $column_name =~ /^Date$/i );
+            $columns{'Date'} = $iC if( $column_name =~ /^Date de diffusion$/i );
+            $columns{'Time'} = $iC if( $column_name =~ /Programme Start Time/i );
+            $columns{'Time'} = $iC if( $column_name =~ /^Start Time/i );
+            $columns{'Time'} = $iC if( $column_name =~ /Programme Start$/i );
+            $columns{'Time'} = $iC if( $column_name =~ /^Heure de diffusion$/i );
+            $columns{'Duration'} = $iC if( $column_name =~ /Programme Duration/i );
+            $columns{'Duration'} = $iC if( $column_name =~ /Durée/i );
+            $columns{'Synopsis'} = $iC if( $column_name =~ /Programme Synopsis Txt ENG/i );
+            $columns{'Synopsis'} = $iC if( $column_name =~ /Programme Synopsis Txt$/i );
+            $columns{'Synopsis'} = $iC if( $column_name =~ /^Synopsis$/i );
+            $columns{'Synopsis'} = $iC if( $column_name =~ /^EPG Synopsis$/i );
+            $columns{'Synopsis'} = $iC if( $column_name =~ /^EPG sysnopsis$/i );
+            $columns{'Synopsis'} = $iC if( $column_name =~ /^Synopsis du Programme$/i );
+            $columns{'EpNum'} = $iC if( $column_name =~ /^Episode Number$/i );
+            $columns{'EpsNum'} = $iC if( $column_name =~ /^Episodes in the season$/i );
+            $columns{'Rating'} = $iC if( $column_name =~ /^PG Rating$/i );
+            $columns{'Rating'} = $iC if( $column_name =~ /^Rating$/i );
+            $columns{'Rating'} = $iC if( $column_name =~ /^Programme Rating$/i );
+            $columns{'Genre'} = $iC if( $column_name =~ /^Genre$/i );
+
+            $foundcolumns = 1 if( $column_name =~ /Date/i );
           }
         }
-#foreach my $cl (%columns) {
-#print "$cl\n";
-#}
 
-        # the number of columns must be 8 or more
-        if( keys( %columns ) < 8 ){
-          undef %columns;
-        }
+        %columns = () if( $foundcolumns eq 0 );
 
         next;
       }
-      
+
+      print Dumper(%columns);
+
       # Date
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'programmeDate DD/MM/YY'}];
+      next if( !defined($columns{'Date'}) );
+      $oWkC = $oWkS->{Cells}[$iR][$columns{'Date'}];
       next if( ! $oWkC );
       next if( ! $oWkC->Value );
-      $date = ParseDate( $oWkC->Value );
+      my $date = ExcelFmt('yyyy-MM-dd', $oWkC->Value);
       next if( ! $date );
 
-      if( $date ne $currdate ) {
-        if( $currdate ne "x" ) {
-	  $dsh->EndBatch( 1 );
-        }
-
-        my $batch_id = $channel_xmltvid . "_" . $date;
-        $dsh->StartBatch( $batch_id , $channel_id );
-        $dsh->StartDate( $date , "06:00" );
-        $currdate = $date;
-
-        progress("Trace XLS: $channel_xmltvid: Date is: $date");
-      }
-
       # Time
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'Programme Start Time'}];
+      $oWkC = $oWkS->{Cells}[$iR][$columns{'Time'}];
       next if( ! $oWkC );
       next if( ! $oWkC->Value );
-      my $time = ParseTime( $oWkC->Value );
-      next if( ! $time );
-
-      # Title
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'programmeName'}];
-      next if( ! $oWkC );
-      next if( ! $oWkC->Value );
-      my $title = $oWkC->Value;
-      next if( ! $title );
+      my $time = ExcelFmt('hh:mm', $oWkC->{Val});
 
       # Duration
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'programme Duration'}];
-      next if( ! $oWkC );
-      next if( ! $oWkC->Value );
-      my $duration = $oWkC->Value;
+      my $duration = undef;
+      if(defined($columns{'Duration'})) {
+        $oWkC = $oWkS->{Cells}[$iR][$columns{'Duration'}];
+        $duration = ExcelFmt('hh:mm', $oWkC->{Val});
+      }
 
-      # Synopsis
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'programme Synopsis Txt'}];
-      next if( ! $oWkC );
-      next if( ! $oWkC->Value );
-      my $synopsis = $oWkC->Value;
+      # Parse
+      my( $start_dt , $end_dt ) = create_dt( $date , $time , $duration );
+
+      # Create date
+      if( $start_dt->ymd("-") ne $currdate ) {
+        if( $currdate ne "x" ) {
+	         $dsh->EndBatch( 1 );
+        }
+
+        my $batch_id = $chd->{xmltvid} . "_" . $start_dt->ymd("-");
+        $dsh->StartBatch( $batch_id , $chd->{id} );
+        $dsh->StartDate( $start_dt->ymd("-") , "06:00" );
+        $currdate = $start_dt->ymd("-");
+
+        progress("Trace XLS: $chd->{xmltvid}: Date is: " . $start_dt->ymd("-"));
+      }
+
+      # Title
+      next if( !defined($columns{'Title'}) );
+      my $title_field = int2col($columns{'Title'}).$i;
+      my $title = $ref->[$iSheet+1]{$title_field};
+
+      print Dumper($title);
 
       # Rating
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'programme Rating'}];
-      next if( ! $oWkC );
-      my $rating = $oWkC->Value;
-
-      # Language
-      $oWkC = $oWkS->{Cells}[$iR][$columns{'programme Language'}];
-      next if( ! $oWkC );
-      my $language = $oWkC->Value;
-
-      progress( "Trace XLS: $channel_xmltvid: $time - $title" );
+      if(defined($columns{'Rating'})) {
+        $oWkC = $oWkS->{Cells}[$iR][$columns{'Rating'}];
+        next if( ! $oWkC );
+        my $rating = $oWkC->Value;
+      }
 
       my $ce = {
-        channel_id => $channel_id,
+        channel_id => $chd->{id},
         title => $title,
-        start_time => $time,
+        start_time => $start_dt->hms(":"),
       };
 
-      $ce->{description} = $synopsis if $synopsis;
+      $ce->{end_time} = $end_dt->hms(":") if defined($end_dt);
+
+      # Synopsis
+      if(defined($columns{'Synopsis'})) {
+        $oWkC = $oWkS->{Cells}[$iR][$columns{'Synopsis'}];
+        my $synopsis_field = int2col($columns{'Synopsis'}).$i;
+        my $synopsis = $ref->[$iSheet+1]{$synopsis_field};
+        $ce->{description} = norm($synopsis) if $synopsis;
+      }
+
+      # EpTitle
+      if(defined($columns{'EpTitle'})) {
+        $oWkC = $oWkS->{Cells}[$iR][$columns{'EpTitle'}];
+        my $subtitle_field = int2col($columns{'EpTitle'}).$i;
+        my $subtitle = $ref->[$iSheet+1]{$subtitle_field};
+        $ce->{subtitle} = norm($subtitle) if $subtitle;
+      }
+
+      # Episodes
+      if(defined($columns{'EpNum'})) {
+        $oWkC = $oWkS->{Cells}[$iR][$columns{'EpNum'}];
+        my $episode = $oWkC->Value;
+
+        # Eps
+        if(defined($columns{'EpsNum'})) {
+          $oWkC = $oWkS->{Cells}[$iR][$columns{'EpsNum'}];
+          my $episodes = $oWkC->Value;
+          $ce->{episode} = sprintf( " . %d/%d . ", $episode-1, $episodes );
+        } else {
+          $ce->{episode} = sprintf( " . %d . ", $episode-1 );
+        }
+      }
+
+      if(defined($columns{'Genre'})) {
+        $oWkC = $oWkS->{Cells}[$iR][$columns{'Genre'}];
+        my $genre_field = int2col($columns{'Genre'}).$i;
+        my $genre = $ref->[$iSheet+1]{$genre_field};
+
+        my ( $program_type, $category ) = $self->{datastore}->LookupCat( "Trace", $genre );
+        #AddCategory( $ce, $program_type, $category );
+      }
+
+      print Dumper($ce);
+
+      progress( "Trace XLS: $chd->{xmltvid}: $start_dt - $title" );
 
       $dsh->AddProgramme( $ce );
 
@@ -207,118 +284,55 @@ sub ImportXLS
   return;
 }
 
-sub ParseDate
+sub create_dt
 {
-  my( $text ) = @_;
+  my( $date, $time , $du ) = @_;
+  return undef if(!$date or !$time);
 
-  return undef if not $text;
-#print ">$text<\n";
+  print("$date - $time - $du\n");
 
   my( $day, $month, $year );
 
   # Format '2010-03-31'
-  if( $text =~ /^\d{4}-\d{2}-\d{2}$/ ){
-    ( $year, $month, $day ) = ( $text =~ /^(\d+)-(\d+)-(\d+)$/ );
+  if( $date =~ /^\d{4}-\d{2}-\d{2}$/ ){
+    ( $year, $month, $day ) = ( $date =~ /^(\d+)-(\d+)-(\d+)$/ );
   # Format 'DD/MM/YY'
-  } elsif( $text =~ /^\d+\/\d+\/\d+$/ ){
-    ( $day, $month, $year ) = ( $text =~ /^(\d+)\/(\d+)\/(\d+)$/ );
+  } elsif( $date =~ /^\d+\/\d+\/\d+$/ ){
+    ( $day, $month, $year ) = ( $date =~ /^(\d+)\/(\d+)\/(\d+)$/ );
   # Format 'MM-DD-YY'
-  } elsif( $text =~ /^\d+-\d+-\d+$/ ){
-    ( $month, $day, $year ) = ( $text =~ /^(\d+)-(\d+)-(\d+)$/ );
+  } elsif( $date =~ /^\d+-\d+-\d+$/ ){
+    ( $month, $day, $year ) = ( $date =~ /^(\d+)-(\d+)-(\d+)$/ );
   # Format '40179' - Excel date format
-  } elsif( $text =~ /^\d{5}$/ ){
-    my $dt = DateTime::Format::Excel->parse_datetime( $text );
+  } elsif( $date =~ /^\d{5}$/ ){
+    my $dt = DateTime::Format::Excel->parse_datetime( $date );
     $year = $dt->year;
     $month = $dt->month;
     $day = $dt->day;
   }
 
-  $year += 2000 if $year < 100;
+  return undef if not $year;
 
-  return sprintf( "%04d-%02d-%02d", $year, $month, $day );
-}
+  # start time
+  my ( $hour , $minute ) = ( $time =~ /^(\d+)\:(\d+)/ );
+  my $sdt = DateTime->new( year   => $year,
+                           month  => $month,
+                           day    => $day,
+                           hour   => $hour,
+                           minute => $minute,
+                           second => 0,
+                           time_zone => 'Europe/Paris',
+                           );
+  # times are in CET timezone in original file
+  #$sdt->set_time_zone( "UTC" );
 
-sub ParseTime
-{
-  my( $text ) = @_;
-
-  return undef if not $text;
-#print ">$text<\n";
-
-  my( $hour, $min, $sec );
-
-  # Format '21:00'
-  if( $text =~ /^\d+:\d+$/ ){
-    ( $hour, $min ) = ( $text =~ /^(\d+):(\d+)$/ );
-  } elsif( $text =~ /^0\.\d+$/){ # format '0.377962962962964'
-    my $daysecs = int( 86400 * $text );
-    $hour = int( $daysecs / 3600 );
-    $min =  int( ( $daysecs - ( $hour * 3600 ) ) / 60 );
+  # end time
+  my $edt = undef;
+  if($du and $du =~ /\:/) {
+    my ( $duhour , $duminute ) = ( $du =~ /^(\d+)\:(\d+)/ );
+    $edt = $sdt->clone->add( hours => $duhour, minutes => $duminute );
   }
 
-  return sprintf( "%02d:%02d", $hour, $min );
-}
-
-sub UpdateFiles {
-  my( $self ) = @_;
-
-#return;
-
-  # get current month name
-  my $year = DateTime->today->strftime( '%g' );
-
-  # the url to fetch data from
-  # is in the format http://tracemovies.com/communication/EPG/INTERNATIONAL/TRACE_INTL_EPG_APR09.xls
-  # UrlRoot = http://tracemovies.com/communication/EPG/INTERNATIONAL/
-  # GrabberInfo = <empty>
-
-  foreach my $data ( @{$self->ListChannels()} ) {
-
-    my $xmltvid = $data->{xmltvid};
-
-    my $today = DateTime->today;
-
-    # do it for MaxMonths in advance
-    for(my $month=0; $month <= $self->{MaxMonths} ; $month++) {
-
-      my $dt = $today->clone->add( months => $month );
-
-      my ( $filename, $url );
-
-      # format: 'TRACE_INTL_EPG_APR09.xls'
-      $filename = "TRACE_INTL_EPG_" . uc( $dt->strftime( '%b' ) ) . uc( $dt->strftime( '%g' ) ) . ".xls";
-      $url = $self->{UrlRoot} . "/" . $filename;
-      progress("Trace: $xmltvid: Fetching xls file from $url");
-#      url_get( $url, $self->{FileStore} . '/' .  $xmltvid . '/' . $filename );
-
-      # format: 'TRACE_INTL_EPG_APR_09.xls'
-#      $filename = "TRACE_INTL_EPG_" . uc( $dt->strftime( '%b' ) ) . "_" . uc( $dt->strftime( '%g' ) ) . ".xls";
-#      $url = $self->{UrlRoot} . "/" . $filename;
-#      progress("Trace: $xmltvid: Fetching xls file from $url");
-#      url_get( $url, $self->{FileStore} . '/' .  $xmltvid . '/' . $filename );
-
-      # format: 'TRACE_INTL_EPG_APRIL09.xls'
-#      $filename = "TRACE_INTL_EPG_" . uc( $dt->strftime( '%B' ) ) . uc( $dt->strftime( '%g' ) ) . ".xls";
-#      $url = $self->{UrlRoot} . "/" . $filename;
-#      progress("Trace: $xmltvid: Fetching xls file from $url");
-#      url_get( $url, $self->{FileStore} . '/' .  $xmltvid . '/' . $filename );
-
-      # format: 'TRACE_INTL_EPG_APRIL_09.xls'
-#      $filename = "TRACE_INTL_EPG_" . uc( $dt->strftime( '%B' ) ) . "_" . uc( $dt->strftime( '%g' ) ) . ".xls";
-#      $url = $self->{UrlRoot} . "/" . $filename;
-#      progress("Trace: $xmltvid: Fetching xls file from $url");
-#      url_get( $url, $self->{FileStore} . '/' .  $xmltvid . '/' . $filename );
-
-    }
-  }
-}
-
-sub url_get {
-  my( $url, $file ) = @_;
-#print "URL: $url\n";
-#print "FILE: $file\n";
-
-  qx[curl -S -s -z "$file" -o "$file" "$url"];
+  return( $sdt, $edt );
 }
 
 1;
