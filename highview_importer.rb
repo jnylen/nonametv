8f495588ee1f4b38e346de55044748e5f30e14f3
Jnylen <me@jnylen.nu>
2017-07-25 20:24:47 +0200

    Fix Highview importer

--- highview_importer.rb
+++ highview_importer.rb
@@ -14,73 +14,93 @@ a = Mechanize.new { |agent|
   agent.user_agent_alias = (Mechanize::AGENT_ALIASES.keys - ['Mechanize']).sample
 }
 
-puts "Fetching file list page..."
-a.get('http://www.highview.com/programminfo.html') do |home_page|
-  #begin
-    # DELUXE
-    deluxe_files = home_page.links_with(:href => /\/Programminfos\/DELUXE\//)
-    deluxe_files.each do |link|
-      file_name = Pathname.new(link.href).basename.to_s
+puts "Fetching login page..."
+a.get('http://www.highview.com/presse/login/login/nc.html') do |home_page|
+  puts "Logging in.."
+  my_page = home_page.form do |form|
+    form['user']  = ENV['HV_LOGIN']
+    form['pass'] = ENV['HV_PASS']
+  end.submit
 
-      if File.exist?('/content/channels/deluxemusic.tv/' + file_name)
-        puts "Downloading http://www.highview.com#{link.href}.."
-        File.open('/tmp/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
+  # Deluxe music
+  deluxe_page = my_page.link_with(:href => '/presse/deluxe-music.html').click
+  deluxe_files = deluxe_page.links_with(:href => /\/fileadmin\/Webdata\/Presseseite\/Listings\/DELUXE\//)
+  deluxe_files.each do |link|
+    file_name = Pathname.new(link.href).basename.to_s
 
-        # Check if it's changed or not
-        if !FileUtils.compare_file('/tmp/' + file_name, '/content/channels/deluxemusic.tv/' + file_name)
-          FileUtils.rm('/content/channels/deluxemusic.tv/' + file_name)
-          FileUtils.mv('/tmp/' + file_name, '/content/channels/deluxemusic.tv/' + file_name)
+    if File.exist?('/content/channels/deluxemusic.tv/' + file_name)
+      puts "Downloading http://www.highview.com#{link.href}.."
+      File.open('/tmp/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
 
-          puts "Updated #{file_name}"
+      # Check if it's changed or not
+      if !FileUtils.compare_file('/tmp/' + file_name, '/content/channels/deluxemusic.tv/' + file_name)
+        FileUtils.rm('/content/channels/deluxemusic.tv/' + file_name)
+        FileUtils.mv('/tmp/' + file_name, '/content/channels/deluxemusic.tv/' + file_name)
 
-          # Channels
-          #copy_to_channels('/content/channels/deluxemusic.tv/' + file_name)
-        else
-          FileUtils.rm('/tmp/' + file_name)
-          puts "Not changed #{file_name}"
-        end
+        puts "Updated #{file_name}"
       else
-        puts "Downloading http://www.highview.com#{link.href}.."
-        File.open('/content/channels/deluxemusic.tv/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
-        puts "Downloaded #{file_name}"
-
-        # Channels
-        #copy_to_channels('/content/channels/deluxemusic.tv/' + file_name)
+        FileUtils.rm('/tmp/' + file_name)
+        puts "Not changed #{file_name}"
       end
+    else
+      puts "Downloading http://www.highview.com#{link.href}.."
+      File.open('/content/channels/deluxemusic.tv/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
+      puts "Downloaded #{file_name}"
     end
+  end
 
-    # RCK
-    rck_files = home_page.links_with(:href => /\/Programminfos\/RCK\//)
-    rck_files.each do |link|
-      file_name = Pathname.new(link.href).basename.to_s
+  # RCK
+  rck_page = my_page.link_with(:href => '/presse/rck-tv.html').click
+  rck_files = rck_page.links_with(:href => /\/fileadmin\/Webdata\/Presseseite\/Listings\/RCK\//)
+  rck_files.each do |link|
+    file_name = Pathname.new(link.href).basename.to_s
 
-      if File.exist?('/content/channels/rck-tv.de/' + file_name)
-        puts "Downloading http://www.highview.com#{link.href}.."
-        File.open('/tmp/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
+    if File.exist?('/content/channels/rck-tv.de/' + file_name)
+      puts "Downloading http://www.highview.com#{link.href}.."
+      File.open('/tmp/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
 
-        # Check if it's changed or not
-        if !FileUtils.compare_file('/tmp/' + file_name, '/content/channels/rck-tv.de/' + file_name)
-          FileUtils.rm('/content/channels/rck-tv.de/' + file_name)
-          FileUtils.mv('/tmp/' + file_name, '/content/channels/rck-tv.de/' + file_name)
+      # Check if it's changed or not
+      if !FileUtils.compare_file('/tmp/' + file_name, '/content/channels/rck-tv.de/' + file_name)
+        FileUtils.rm('/content/channels/rck-tv.de/' + file_name)
+        FileUtils.mv('/tmp/' + file_name, '/content/channels/rck-tv.de/' + file_name)
 
-          puts "Updated #{file_name}"
-
-          # Channels
-          #copy_to_channels('/content/channels/rck-tv.de/' + file_name)
-        else
-          FileUtils.rm('/tmp/' + file_name)
-          puts "Not changed #{file_name}"
-        end
+        puts "Updated #{file_name}"
       else
-        puts "Downloading http://www.highview.com#{link.href}.."
-        File.open('/content/channels/rck-tv.de/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
-        puts "Downloaded #{file_name}"
-
-        # Channels
-        #copy_to_channels('/content/channels/rck-tv.de/' + file_name)
+        FileUtils.rm('/tmp/' + file_name)
+        puts "Not changed #{file_name}"
       end
+    else
+      puts "Downloading http://www.highview.com#{link.href}.."
+      File.open('/content/channels/rck-tv.de/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
+      puts "Downloaded #{file_name}"
     end
+  end
 
-  #rescue
-  #end
+  # Planet TV
+  planet_page = my_page.link_with(:href => '/presse/planet.html').click
+  planet_files = planet_page.links_with(:href => /\/fileadmin\/Webdata\/Presseseite\/Bilder\/Planet\/Listings\//)
+  planet_files.each do |link|
+    file_name = Pathname.new(link.href).basename.to_s
+
+    if File.exist?('/content/channels/planet-tv.de/' + file_name)
+      puts "Downloading http://www.highview.com#{link.href}.."
+      File.open('/tmp/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
+
+      # Check if it's changed or not
+      if !FileUtils.compare_file('/tmp/' + file_name, '/content/channels/planet-tv.de/' + file_name)
+        FileUtils.rm('/content/channels/planet-tv.de/' + file_name)
+        FileUtils.mv('/tmp/' + file_name, '/content/channels/planet-tv.de/' + file_name)
+
+        puts "Updated #{file_name}"
+      else
+        FileUtils.rm('/tmp/' + file_name)
+        puts "Not changed #{file_name}"
+      end
+    else
+      puts "Downloading http://www.highview.com#{link.href}.."
+      File.open('/content/channels/planet-tv.de/' + file_name, 'wb'){ |f| f << a.get('http://www.highview.com' + link.href).body.to_s }
+      puts "Downloaded #{file_name}"
+    end
+  end
+
 end
